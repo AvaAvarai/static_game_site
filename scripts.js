d162aee855b87138b093797c3b0537b10bd07b04
Alice Williams <alice.williams.tech@gmail.com>
2023-04-22 10:57:16 -0700

    basic website files

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener('keydown', function(event) {
+  if (event.keyCode == 37) {
+    alert('left pressed');
+  }
+  else if (event.keyCode == 39) {
+    alert('right pressed');
+  }
+
+});
